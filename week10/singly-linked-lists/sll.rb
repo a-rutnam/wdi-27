3ad0eed9aa1fb1d66833a833a1cb2a03f24034e4
Luke Hammer <textchimp@gmail.com>
2018-06-25 17:35:32 +1000

    filled in methods, refactored with .each, and finally using Enumerable mixin

--- week10/singly-linked-lists/sll.rb
+++ week10/singly-linked-lists/sll.rb
@@ -7,6 +7,8 @@ require 'pry'
 
 class SinglyLinkedList
 
+  include Enumerable
+
   attr_accessor :head
 
   class Node
@@ -20,9 +22,11 @@ class SinglyLinkedList
   end  # Node
 
 
-  def initialize( value )
+  def initialize( value=nil )
     # create a new instance of the Node class, pass on the value to it, and set it as the @head of the list
-    @head = Node.new( value )
+    # (make the value optional so methods like reverse
+    # are easier to write)
+    @head = Node.new( value ) if value
   end
 
   # equivalent of .push(value)
@@ -37,6 +41,8 @@ class SinglyLinkedList
     @head = new_node
   end
 
+  alias_method :unshift, :prepend
+
   def last
     node = @head
     while node && node.next
@@ -47,10 +53,14 @@ class SinglyLinkedList
 
   def to_s
     output = ''
-    node = @head
-    while node
-      output += node.value  + ', '
-      node = node.next
+    # node = @head
+    # while node
+    #   output += node.value  + ', '
+    #   node = node.next
+    # end
+    each do |node|
+      output += node.value
+      output += ', ' unless node.next == nil
     end
     output
   end
@@ -61,34 +71,111 @@ class SinglyLinkedList
 
   def find( needle )
     # returns Node object whose value == needle
+    # node = @head
+    # while node
+    #   return node if node.value == needle
+    #   node = node.next
+    # end
+    each { |node| return node if node.value == needle }
   end
 
   def shift
     # removes the first element of the list and returns its value (destructive, i.e. changes list)
+    old_head = @head
+    @head = @head.next
+    old_head.next = nil # break connection to the list
+    old_head
   end
+
+
+  def length
+    # returns the length of the list
+    # node = @head
+    # n = 0
+    # while node
+    #   n += 1
+    #   node = node.next
+    # end
+    n = 0
+    each { n+= 1 }
+    n
+  end
+
 
   def insert_after( node, value )
     # insert a new node after the given node argument, with the given value
     # returns new node
+    new_node = Node.new value
+    new_node.next = node.next
+    node.next = new_node
   end
 
-  def length
-    # returns the length of the list
+  def to_a
+    # turn our SLL into an array of values and return it
+    arr = []
+    # node = @head
+    # while node
+    #   arr << node.value
+    #   node = node.next
+    # end
+    each { |node| arr << node.value }
+    arr
+  end
+
+  def array_to_list( arr )
+    # ?????????
   end
 
   def reverse
     # return a reversed version of the list, without changing list itself
+
+    # one way to avoid the dangling 'nil':
+    # reversed = SinglyLinkedList.new @head.value
+    # node = @head.next
+
+    # this requires the value for the initialize method to be optional
+    # reversed = SinglyLinkedList.new
+    # node = @head
+    # while node
+    #   # take each node from the current list in forward
+    #   # order, and prepend it to the new list -
+    #   # automatically reverses!
+    #   reversed.prepend node.value
+    #   node = node.next
+    # end
+    reversed = SinglyLinkedList.new
+    each { |node| reversed.prepend node.value }
+    reversed
   end
 
   def reverse!
     # destructive version: returns a reversed version of list, and sets list itself to that
+    @head = reverse.head
   end
 
   def each
     # takes a block, and applies block to each node in list (i.e. node is provided as goalpost arg to block)
     # HINT: look up 'yield'
+    return nil unless block_given?
+    node = @head
+    index = 0
+    while node
+      yield node, index  # node becomes first goalpost arg to block
+      node = node.next
+      index += 1
+    end
   end
 
+  def at_index( n )
+    each { |node, index| return node if index == n }
+  end
+
+  # def map
+  #   return nil unless block_given?
+  #   arr = []  # TODO: return a list instead?
+  #   each { |node| arr << yield(node)  }
+  #   arr
+  # end
 
   # bonus for the showoffs: map, inject
 
